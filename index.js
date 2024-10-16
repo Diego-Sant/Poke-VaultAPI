f03073f0b50111ad5ddc22472b6527d4be4cad0c
Diego de Souza Santana <diegossantana068@gmail.com>
2024-10-16 18:52:13 -0300

    Acess-Control and localhost instead of .env

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 
 import cardRoute from "./routes/card.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -9,15 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: "http://localhost:4200",
   optionsSuccessStatus: 204,
   credentials: true,
   methods: "GET, POST, PUT, DELETE",
 };
 
 app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
-app.use(cookieParser())
 
 app.use("/api/cartas", cardRoute);
 app.use("/api/auth", authRoute);
